aa76a4d21c9857e06c5c349dab01a4806b4ae6f6
Ishu Mehta <mehta.ishu14@gmail.com>
2026-01-03 17:41:48 +0530

    Initial commit

---
+++ src/main/java/com/example/springmvcmaven/SpringMvcMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcMavenApplication.class, args);
+    }
+
+}

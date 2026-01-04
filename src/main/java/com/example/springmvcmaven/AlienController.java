41ca69a54244d72a6b92d62acc9ae07e5bc85919
Ishu Mehta <mehta.ishu14@gmail.com>
2026-01-04 19:52:05 +0530

    @ModelAttribute annotation

--- src/main/java/com/example/springmvcmaven/AlienController.java
+++ src/main/java/com/example/springmvcmaven/AlienController.java
@@ -3,16 +3,17 @@ package com.example.springmvcmaven;
 import com.example.springmvcmaven.model.Alien;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/alien")
 public class AlienController {
 
+    @ModelAttribute
+    public void modelData(Model m) {
+        m.addAttribute("welcomeMessage", "Aliens");
+    }
     @GetMapping()
     public String home() {
         System.out.println("Alien Home Arrived !");
